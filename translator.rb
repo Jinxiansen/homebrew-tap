f2070d31b1420dc6905d51b9101c12627f79ea44
晋先森 <hi@jinxiansen.com>
2019-09-03 10:24:36 +0800

    # Update .rb

--- translator.rb
+++ translator.rb
@@ -3,8 +3,8 @@ class Translator < Formula
     sha256 '3b39ca436dd97c1bbc11008f6a00c6268408a72fbb94d61752deaf57120b7d65'
 
     url "https://github.com/Jinxiansen/Translator/releases/download/#{version}/Translator.app.zip"
-    appcast 'https://github.com/Jinxiansen/Translator/releases.atom'
-    name 'Translator'
+#    appcast 'https://github.com/Jinxiansen/Translator/releases.atom'
+#    name 'Translator'
     homepage 'https://github.com/Jinxiansen/Translator/'
 
 #    auto_updates true
